f6ae1cfe098805e6b360be5f829991dbf9677a0a
quangnvo <voquang.usth@gmail.com>
2023-05-23 21:55:38 +0800

    Change the margin padding

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,10 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center font-semibold">
-      <h1 className='text-5xl'>Hi, I'm Quang 🧑🏻‍💻</h1>
+    <main>
+      <div className='w-full max-w-7xl mx-auto flex items-center justify-center font-semibold '>
+        <h1 className='text-2xl lg:text-4xl'>Hi, I'm Quang 👨🏻‍💻</h1>
+      </div>
     </main>
   )
 }
