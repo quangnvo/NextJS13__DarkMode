8856f37bce8eeb899ab2fe8711985fa8ac8a1f95
quangnvo <voquang.usth@gmail.com>
2023-05-24 11:22:17 +0800

    Add more text

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,9 @@ export default function Home() {
           <h1 className='text-2xl lg:text-4xl text-black dark:text-gray-200'>
             Hi, I'm Quang 👨🏻‍💻
           </h1>
+          <div className="mt-7 text-center py-4  border border-black shadow-lg rounded-md  dark:bg-orange-100 dark:text-black">
+            Random text
+          </div>
         </div>
       </div>
     </main>
